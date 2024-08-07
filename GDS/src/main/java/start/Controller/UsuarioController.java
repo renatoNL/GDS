6d1ae9d472167b2246a1916bea71a9036db8fbc8
leticia oliveira <leticiaoliveiraju@gmail.com>
2024-08-07 11:43:18 -0300

    feat update e delete
    adicionado update e delete no projeto

--- GDS/src/main/java/start/Controller/UsuarioController.java
+++ GDS/src/main/java/start/Controller/UsuarioController.java
@@ -3,8 +3,11 @@ package start.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,21 @@ public class UsuarioController {
     @PostMapping
     public UsuarioModel CriarUsuario(@RequestBody UsuarioModel usario){
         return usuarioService.criarUsuario(usario);
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioModel getUsuarioById(@PathVariable Long id) {
+        return usuarioService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioModel updateUsuario(@PathVariable Long id, @RequestBody UsuarioModel usuarioAtualizado) {
+        return usuarioService.updateUsuario(id, usuarioAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public void DeletarUsiario(@PathVariable Long id){
+        usuarioService.deletarUsuario(id);
     }
 
 }
--- GDS/src/main/java/start/Service/UsuarioService.java
+++ GDS/src/main/java/start/Service/UsuarioService.java
@@ -1,9 +1,10 @@
 package start.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import start.Repository.UsuarioRepository;
 import start.Model.UsuarioModel;
 
@@ -19,6 +20,30 @@ public class UsuarioService {
 
     public UsuarioModel criarUsuario (UsuarioModel usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    public UsuarioModel findById(Long id) {
+        Optional<UsuarioModel> usuario = usuarioRepository.findById(id);
+        return usuario.orElse(null);
+    }
+
+    public UsuarioModel updateUsuario(Long id, UsuarioModel usuarioAtualizado) {
+        Optional<UsuarioModel> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isPresent()) {
+            UsuarioModel usuarioExistente = usuarioOptional.get();
+            usuarioExistente.setNome(usuarioAtualizado.getNome());
+            usuarioExistente.setCpf(usuarioAtualizado.getCpf());
+            usuarioExistente.setDataNascimento(usuarioAtualizado.getDataNascimento());
+            usuarioExistente.setCelular(usuarioAtualizado.getCelular());
+            usuarioExistente.setEmail(usuarioAtualizado.getEmail());
+            usuarioExistente.setSenha(usuarioAtualizado.getSenha());
+            return usuarioRepository.save(usuarioExistente);
+        }
+        return null;
+    }
+
+    public void deletarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
     }
 
 }
