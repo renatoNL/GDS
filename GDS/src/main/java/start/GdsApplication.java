bfca4d8c80f16d1d28dcc251ad7c4d290aa325b8
leticia oliveira <leticiaoliveiraju@gmail.com>
2024-08-06 21:15:50 -0300

    feat back end
    adicionando back end

---
+++ GDS/src/main/java/start/GdsApplication.java
@@ -0,0 +1,13 @@
+package start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdsApplication.class, args);
+	}
+
+}
---
+++ GDS/src/test/java/start/GdsApplicationTests.java
@@ -0,0 +1,13 @@
+package start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
